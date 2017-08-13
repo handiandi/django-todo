ab6af093f5791e2825e448522dba932c0627ce65
Anders <handiiandii@gmail.com>
2017-08-13 21:04:21 +0200

    updated. Cookie plugin and done/un-done works

--- mysite/todo/static/js/todo.js
+++ mysite/todo/static/js/todo.js
@@ -30,10 +30,12 @@ $( document ).ready(
                             <div class=\"row align-middle\">\
                                 <div class=\"col-sm align-middle\" style=\"background-color: #f0f0f0; height: 50px;\">" + title + "</div>\
                                 <div class=\"col-sm align-middle\" style=\"background-color: #f0f0f0; height: 50px;\">" + descr + "</div>\
-                                <div class=\"col-sm align-middle\" style=\"background-color: #f0f0f0; height: 50px;\"><input type=\"checkbox\" id=" + title + "></div>\
+                                <div class=\"col-sm align-middle\" style=\"background-color: #f0f0f0; height: 50px;\"><input type=\"checkbox\" id=" + title + " name=\"undone\"></div>\
                             </div>\
                         </div>";
-                $(html).insertAfter($('[name="to-do-container"]').last());
+                //$(html).insertAfter($('[name="to-do-container"]').last());
+                $('[name="p no todos"]' ).remove();
+                $(html).appendTo($('[name="undone-to-do-container"]'));
             }
         });
     });
@@ -45,34 +47,30 @@ function checkTodo() {
         titlesValues = $("input[name='undone']:checkbox:checked").map(function(){
             return $(this).attr('id');
         }).get();
-
-        alert(titlesValues.length); //$("input[name='undone']:checkbox:checked").length);
-
-        alert($("[name='csrfmiddlewaretoken']").val());
-
+        csrftoken = Cookies.get('csrftoken');
         $.ajax({
             //url: "",
-            method: 'PUT', // or another (GET), whatever you need
+            method: 'POST', // or another (GET), whatever you need
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            },
             data: {
                 titles: titlesValues, // data you need to pass to your function
-                //csrfmiddlewaretoken: $("[name='csrfmiddlewaretoken']").val(),
                 check: true
             },
-            dataType: "json",
-            success: function (data) { 
-                title = data[0].fields.title;
-                descr = data[0].fields.description;
-                html = " <div class=\"container\" name=\"to-do-container\">\
-                            <div class=\"row align-middle\">\
-                                <div class=\"col-sm align-middle\" style=\"background-color: #f0f0f0; height: 50px;\">" + title + "</div>\
-                                <div class=\"col-sm align-middle\" style=\"background-color: #f0f0f0; height: 50px;\">" + descr + "</div>\
-                                <div class=\"col-sm align-middle\" style=\"background-color: #f0f0f0; height: 50px;\"><input type=\"checkbox\" id=" + title + " name=\"undone\"></div>\
-                            </div>\
-                        </div>";
-                $(html).insertAfter($('[name="to-do-container"]').last());
+
+            //dataType: "json",
+            success: function (data) {
+                location.reload(); 
             }
         });
     });
  }
 
 
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from django.http import HttpResponse, JsonResponse
 from todo.models import todo
-import datetime, json
+from django.db.models import Case, Value, When
+import datetime
+import json
 from django.core import serializers
 
-@csrf_protect
+
 def all_todo(request):
     add = request.POST.get('add', False)
-    if request.method == 'POST' and add:  # get('click', False):  # check if called by click
+    # get('click', False):  # check if called by click
+    if request.method == 'POST' and add:
         title = request.POST.get('title')
         description = request.POST.get('description')
         new_todo = todo(title=title, description=description, done=False)
@@ -19,10 +22,27 @@ def all_todo(request):
         print(todos_json)
         return HttpResponse(todos_json, content_type='application/json')
 
-    if request.method == "PUT" and request.PUT.get("check", False):
-        print("PUT!")
+    # print(request.method)
+    print(request.POST)
+    if request.method == "POST" and request.POST.get("check", False):
+        print("POST og check")
+        titles = request.POST.getlist('titles[]', False)
+        print(titles)
+        for title in titles:
+            todo.objects.filter(title=title).update(
+                done=Case(When(done=True, then=Value(False)),
+                          default=Value(True)))
 
-    todos = todo.objects.all()
+            #obj, created = todo.objects.update_or_create(
+            #    title=title, defaults={'done': True})
+            #print(created)
+            # t.save(update_fields=['done'])
+        print(titles)
+        return HttpResponse()
+
+    todos_undone = todo.objects.filter(done=False)
+    todos_done = todo.objects.filter(done=True)
+    # print(todos.values())
     now = datetime.datetime.now()
-    return render(request, 'index.html', {'todos': todos})
+    return render(request, 'index.html', {'todos_undone': todos_undone, 'todos_done': todos_done})
 # Create your views here.
